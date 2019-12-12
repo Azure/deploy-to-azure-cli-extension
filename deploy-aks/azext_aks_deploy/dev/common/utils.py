a039136a0ecca3ad5763f0303d4a8dc1ba8a2561
Ishita Mehta <ishitam@microsoft.com>
2019-12-12 16:58:50 +0530

    Install pyperclip

--- deploy-aks/azext_aks_deploy/dev/common/utils.py
+++ deploy-aks/azext_aks_deploy/dev/common/utils.py
@@ -4,7 +4,17 @@
 # --------------------------------------------------------------------------------------------
 from knack.log import get_logger
 from knack.util import CLIError
+from azure.cli.core.extension import get_extension_path
+from azure.cli.core.extension.operations import _run_pip
 
+PYPERCLIP_MSG = """
+Pyperclip could not find a copy/paste mechanism for your system.
+On Linux, install xclip or xsel via package manager. For example, in Debian:
+    sudo apt-get install xclip
+    sudo apt-get install xsel
+    
+If you're seeing this error on Windows or Mac please visit https://pyperclip.readthedocs.io/en/latest/introduction.html#not-implemented-error
+"""
 logger = get_logger(__name__)
 
 FILE_ENCODING_TYPES = ['ascii', 'utf-16be', 'utf-16le', 'utf-8']
@@ -72,6 +82,27 @@ def singleton(myclass):
         return instance[0]
     return wrapper
 
+
+def copy_to_clipboard(content):
+    try:
+        import pyperclip
+    except ModuleNotFoundError as ex:
+        logger.debug(ex)
+        _install_package(pyperclip)
+    try:
+        pyperclip.copy(content)
+    except pyperclip.PyperclipException as ex:
+        CLIError(PYPERCLIP_MSG)
+
+        
+def _install_package(package_name):
+    logger.debug('Installing pyperclip')
+    extensionPath = get_extension_path('aks-deploy')
+    pip_args = ['install' ,package_name, '--target' , extensionPath]
+    pip_status_code = _run_pip(pip_args)
+    if pip_status_code > 0:
+        raise CLIError('An error occurred. Pip failed with status code {} for package {}. '
+                       'Use --debug for more information.'.format(pip_status_code, package_name))
 
 def _read_file_content_ver3(file_path, encoding):
     logger.debug('inside read_file_content_ver3')
