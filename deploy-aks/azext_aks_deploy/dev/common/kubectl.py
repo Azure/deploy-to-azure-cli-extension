d73b7298c9a9c945d4cf0afa1628a4f1338b3008
Ishita Mehta <ishitam@microsoft.com>
2019-12-24 15:53:03 +0530

    Replace values in yaml

--- deploy-aks/azext_aks_deploy/dev/common/kubectl.py
+++ deploy-aks/azext_aks_deploy/dev/common/kubectl.py
@@ -8,13 +8,14 @@ from azext_aks_deploy.dev.common.utils import which
 
 logger = get_logger(__name__)
 
-def get_deployment_IP_port(app_name):
+def get_deployment_IP_port(release_name, language):
     if not which('kubectl'):
         raise CLIError('Can not find kubectl executable in PATH')
     try:
         import subprocess
         import json
-        service_details = subprocess.check_output('kubectl get service {} -o json'.format(app_name), shell=True)
+        service_name = release_name + '-' +language.lower()
+        service_details = subprocess.check_output('kubectl get service {} -o json'.format(service_name), shell=True)
         service_obj = json.loads(service_details)
         deployment_ip = service_obj['status']['loadBalancer']['ingress'][0]['ip']
         port = service_obj['spec']['ports'][0]['port']
