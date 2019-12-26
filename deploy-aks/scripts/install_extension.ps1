5f001b759949051cfcbdde5aae7fdcf67a6b28a3
Ishita Mehta <ishitam@microsoft.com>
2019-12-26 14:08:23 +0530

    revert

--- deploy-aks/scripts/install_extension.ps1
+++ deploy-aks/scripts/install_extension.ps1
@@ -2,4 +2,4 @@ python .\setup.py bdist_wheel
 
 az extension remove -n aks-deploy
 
-az extension add --source 'D:\VSTS\AKS_CLI\myAksUpRepo\cli-extension-aks-up\deploy-aks\dist\aks_deploy-0.0.2-py2.py3-none-any.whl' -y
+az extension add --source 'D:\GithubRepos\Work\MS\newextension\deploy-aks\dist\aks_deploy-0.0.2-py2.py3-none-any.whl' -y
