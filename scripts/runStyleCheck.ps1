1f6757fa852e640d90ff9a3f0b155e2bb29bd2e6
Atul Bagga <atbagga@microsoft.com>
2020-01-08 15:20:56 +0530

    python path

--- scripts/runStyleCheck.ps1
+++ scripts/runStyleCheck.ps1
@@ -1,10 +1,10 @@
 $failure = $false             
 
 Write-Output "Installing pylint"
-python -m pip install --user pylint
+python3 -m pip install --user pylint
 
 Write-Output "Installing flake8"
-python -m pip install --user flake8
+python3 -m pip install --user flake8
 
 Write-Output "Running pylint on all source"
 pylint --rcfile pylintrc ./deploy-aks/azext_aks_deploy -f colorized
