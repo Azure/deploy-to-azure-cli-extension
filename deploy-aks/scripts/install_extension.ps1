cc5e919cfdc56838b323abc6dd793088eb803afb
Atul Bagga <atbagga@microsoft.com>
2020-01-20 11:35:08 +0530

    Remove utility script from repo

--- deploy-aks/scripts/install_extension.ps1
+++
@@ -1,5 +0,0 @@
-python .\setup.py bdist_wheel
-
-az extension remove -n aks-deploy
-
-az extension add --source 'D:\GithubRepos\Work\MS\newextension\deploy-aks\dist\aks_deploy-0.0.2-py2.py3-none-any.whl' -y
