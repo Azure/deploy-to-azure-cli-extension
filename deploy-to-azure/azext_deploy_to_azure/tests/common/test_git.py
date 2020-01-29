4f2b9a90d50ae09dc1cbb76cbf90017bab9e03bd
Atul Bagga <atbagga@microsoft.com>
2020-01-29 10:36:45 +0530

    Adding sample unit tests and setting up test run in CI

---
+++ deploy-to-azure/azext_deploy_to_azure/tests/common/test_git.py
@@ -0,0 +1,29 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import unittest
+try:
+    # Attempt to load mock (works on Python 3.3 and above)
+    from unittest.mock import patch
+except ImportError:
+    # Attempt to load mock (works on Python version below 3.3)
+    from mock import patch
+from azext_aks_deploy.dev.common.git import is_github_url_candidate
+
+
+class TestGitMethods(unittest.TestCase):
+
+    def test_github_url_candidate(self):
+        self.assertEqual(is_github_url_candidate(url='https://github.com/org/repo'), True)
+        self.assertEqual(is_github_url_candidate(url='https://github.com/org/repo/'), True)
+        self.assertEqual(is_github_url_candidate(url='https://github.com/org'), True)
+        self.assertEqual(is_github_url_candidate(url='https://github.com'), True)
+        self.assertEqual(is_github_url_candidate(url='https://dev.azure.com/org'), False)
+        self.assertEqual(is_github_url_candidate(url='https://github.in/org/repo'), False)
+        self.assertEqual(is_github_url_candidate(url=None), False)
+        self.assertEqual(is_github_url_candidate(url='not a url'), False)
+
+if __name__ == '__main__':
+    unittest.main() 
