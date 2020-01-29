c8d03eece650ba05e2b4c0751afd3e92e9065604
Mitesh Shah <mitsha@microsoft.com>
2020-01-29 15:28:28 +0530

    Argument Context Fix

--- deploy-to-azure/azext_deploy_to_azure/dev/aci/arguments.py
+++ deploy-to-azure/azext_deploy_to_azure/dev/aci/arguments.py
@@ -5,5 +5,5 @@
 
 
 def load_aci_arguments(self, _):
-    with self.argument_conext('aci up') as c:
+    with self.argument_context('aci up') as c:
         c.argument('repository', options_list=['--repository'])
