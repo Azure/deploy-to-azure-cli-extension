cfd60d2c493cc2fa3c559dbdc5671988d3d788f2
Ishita Mehta <43136080+ishitam8@users.noreply.github.com>
2020-02-06 21:16:28 +0530

    Update github_azure_secrets.py

--- deploy-to-azure/azext_deploy_to_azure/dev/common/github_azure_secrets.py
+++ deploy-to-azure/azext_deploy_to_azure/dev/common/github_azure_secrets.py
@@ -15,23 +15,22 @@ def get_azure_credentials(repo_name):
     import json
     _subscription_id, _subscription_name, _tenant_id, _environment_name = get_default_subscription_info()
     print('Creating AZURE_CREDENTIALS secret...')
-    if not check_secret_exists(repo_name, 'AZURE_CREDENTIALS'):
+    if check_secret_exists(repo_name, 'AZURE_CREDENTIALS'):
+        logger.warning('Skipped creating AZURE_CREDENTIALS as it already exists')
+    else:
         auth_details = subprocess.check_output('az ad sp create-for-rbac --sdk-auth -o json', shell=True)
         auth_details_json = json.loads(auth_details)
         auth_details_string = json.dumps(auth_details_json)
         create_repo_secret(repo_name, 'AZURE_CREDENTIALS', auth_details_string)
-    else:
-        logger.warning('Skipped creating AZURE_CREDENTIALS as it already exists')
     print('')
     print('Creating REGISTRY_USERNAME and REGISTRY_PASSWORD...')
-    if not check_secret_exists(repo_name, 'REGISTRY_USERNAME') \
-                               and not check_secret_exists(repo_name, 'REGISTRY_PASSWORD'):
+    if check_secret_exists(repo_name, 'REGISTRY_USERNAME') and check_secret_exists(repo_name, 'REGISTRY_PASSWORD'):
+        logger.warning('Skipped creating REGISTRY_USERNAME and REGISTRY_PASSWORD as it already exists')
+    else:
         sp_details = subprocess.check_output('az ad sp create-for-rbac -o json', shell=True)
         sp_details_json = json.loads(sp_details)
         create_repo_secret(repo_name, 'REGISTRY_USERNAME', sp_details_json['appId'])
         create_repo_secret(repo_name, 'REGISTRY_PASSWORD', sp_details_json['password'])
-    else:
-        logger.warning('Skipped creating REGISTRY_USERNAME and REGISTRY_PASSWORD as it already exists')
 
 
 def get_azure_credentials_functionapp(repo_name, app_name):
@@ -40,12 +39,12 @@ def get_azure_credentials_functionapp(repo_name, app_name):
     _subscription_id, _subscription_name, _tenant_id, _environment_name = get_default_subscription_info()
     print('')
     print('Creating AZURE_CREDENTIALS secret...')
-    if not check_secret_exists(repo_name, 'AZURE_CREDENTIALS'):
+    if check_secret_exists(repo_name, 'AZURE_CREDENTIALS'):
+        logger.warning('Skipped creating AZURE_CREDENTIALS as it already exists')
+    else:
         auth_details = subprocess.check_output(
             'az ad sp create-for-rbac --name {} --role contributor --sdk-auth -o json'.format(app_name),
             shell=True)
         auth_details_json = json.loads(auth_details)
         auth_details_string = json.dumps(auth_details_json)
         create_repo_secret(repo_name, 'AZURE_CREDENTIALS', auth_details_string)
-    else:
-        logger.warning('Skipped creating AZURE_CREDENTIALS as it already exists')
